0c89106742067bd5518420704ca5169ba8324b5f
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2017-08-11 01:07:31 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Estúdio Digital Bocca #
+## Gerador de Tags Exclusivas - Backend NODE ##
+### v0.1.0 ###
+
+>Um gerador de tags exclusivas para seu canal no youtube.
+>Esta Versão é apenas a API que retorna a Tag para o cliente que a implementa.
+
+**Notas da Versão v0.1.0**
+
+- Iniciado o Desenvolvimento e o Controle de Versão.
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * (c)2017 - Estúdio Digital Bocca
+ * https://estudiodigitalbocca.com.br
+ */
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+// const moment = require('moment')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  let dinamico = Date.now()
+  
+  /**
+   * Implementação de Navegador
+   */
+  // let hashBase64 = btoa(dinamico)
+  
+  /**
+   * Implementação em NODE
+   */
+  let hashBase64 = Buffer.from(dinamico.toString()).toString('base64')
+
+  if (hashBase64.length > 10) {
+    hashBase64 = hashBase64.substr(-10)
+  }
+
+  let primeiraParte = hashBase64.substring(0, 5)
+  let segundaParte = hashBase64.substring(5,10)
+
+  // let exclusividade = btoa($('#exclusividade').val())
+  // let exSemEspacos = exclusividade.split(' ').join('').toUpperCase()
+  let algInsert = (dinamico * 344590).toString().substr(-10).substring(0, 5)
+
+  var tagConcatenada = algInsert + primeiraParte + algInsert /* + exSemEspacos */ + algInsert + segundaParte + algInsert
+
+  res.json({tagExclusiva: tagConcatenada})
+})
+
+app.listen(80, function () {
+  console.log('Funcionando na porta 3000')
+})
+// app.listen(process.env.PORT)
