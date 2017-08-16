9560780334304c1d723f2c79d07a70961a832c5c
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2017-08-16 04:01:48 -0300

    Melhorias no Javascript

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# Estúdio Digital Bocca #
+# GTE-API #
 ## Gerador de Tags Exclusivas - Backend NODE ##
-### v0.2.0 ###
+### v0.3.0 ###
 
->Um gerador de tags exclusivas para seu canal no youtube.
->Esta Versão é apenas a API que retorna a Tag para o cliente que a implementa.
+Um gerador de tags exclusivas para seu canal no youtube.
+
+**Problema:** "Reza a lenda" que, tendo uma tag exclusiva nos seus vídeos, o algoritmo do YouTube entrega nas recomendações outros vídeos do seu canal. Todos os vídeos com o mesma tag são interpretados como relacionados. Vamos testar.
+
+Esta Versão é apenas a API que retorna a Tag para o cliente que a implementa.
+
+## DEPLOY com WeDeploy ##
+
+- Crie um Novo Projeto.
+- Altere o nome do Serviço no arquivo `wedeploy.json`.
+- Faça o Deploy: `we deploy -p <seu-projeto>`.
+
+Tudo estará rodando em `<service>-<project>.wedeploy.io`.
+
+## EXEMPLO ##
+
+
+
+## APP QUE UTILIZA ESTA API ##
+
+
+
+## CHANGELOG ##
+
+**Notas da Versão v0.3.0**
+
+- Melhorias no código Javascript.
 
 **Notas da Versão v0.2.0**
 
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 /**
- * (c)2017 - Estúdio Digital Bocca
- * https://estudiodigitalbocca.com.br
+ * @file Cria e entrega uma tag exclusiva.
+ *
+ * @todo Modularizar a criação da tag.
+ * @todo Configurar Porta como uma Environment Variable.
+ *
+ * @author Gabriel Bertola Bocca <gabriel@estudiodigitalbocca.com.br>
+ * @copyright 2017 - Estúdio Digital Bocca - https://estudiodigitalbocca.com.br
  */
 
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-// const moment = require('moment')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -15,15 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   let dinamico = Date.now()
-  
-  /**
-   * Implementação de Navegador
-   */
-  // let hashBase64 = btoa(dinamico)
-  
-  /**
-   * Implementação em NODE
-   */
   let hashBase64 = Buffer.from(dinamico.toString()).toString('base64')
 
   if (hashBase64.length > 10) {
@@ -31,13 +26,10 @@ app.get('/', (req, res) => {
   }
 
   let primeiraParte = hashBase64.substring(0, 5)
-  let segundaParte = hashBase64.substring(5,10)
-
-  // let exclusividade = btoa($('#exclusividade').val())
-  // let exSemEspacos = exclusividade.split(' ').join('').toUpperCase()
+  let segundaParte = hashBase64.substring(5, 10)
   let algInsert = (dinamico * 344590).toString().substr(-10).substring(0, 5)
 
-  var tagConcatenada = algInsert + primeiraParte + algInsert /* + exSemEspacos */ + algInsert + segundaParte + algInsert
+  let tagConcatenada = algInsert + primeiraParte + algInsert + segundaParte + algInsert
 
   res.json({tagExclusiva: tagConcatenada})
 })
@@ -45,4 +37,5 @@ app.get('/', (req, res) => {
 app.listen(80, function () {
   console.log('Funcionando na porta 80')
 })
+
 // app.listen(process.env.PORT)
